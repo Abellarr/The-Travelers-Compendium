f213cb9aaa7c165962716c19ed041285c15cd839
Abellarr <da.ortiz83@gmail.com>
2023-06-10 11:49:32 -0700

    Added some styling for the home page.

--- travelers-compendium/src/components/Body/UnderConstruction.jsx
+++ travelers-compendium/src/components/Body/UnderConstruction.jsx
@@ -4,8 +4,8 @@ const UnderConstruction = () => {
 
 
     return (
-        <div>
-            <img src='https://img.freepik.com/premium-vector/construction-website-page-border-stripe-web-vector-stock-illustration_100456-7028.jpg' alt='Website Under Construction'/>
+        <div className='underConstruction'>
+            <img src='https://img.freepik.com/premium-vector/construction-website-page-border-stripe-web-vector-stock-illustration_100456-7028.jpg' alt='Website Under Construction' className='underConstructionImg'/>
             <h1>This feature is currently under construction</h1>
         </div>
     )
