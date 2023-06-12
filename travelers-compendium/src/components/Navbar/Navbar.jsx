049217aef95dc8ed6b63c671ca90af7ab6de337d
Abellarr <da.ortiz83@gmail.com>
2023-06-12 16:54:43 -0700

    Working with navBar components on main page for styling and adding content lists to accordion.

--- travelers-compendium/src/components/Navbar/Navbar.jsx
+++ travelers-compendium/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,11 @@ import '../../styles/Navbar.css';
 
 const Navbar = () => {
 
+    const handleClick = (e) => {
+        const panel = document.getElementById(e.target.id).nextElementSibling;
+        panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
 
     return (
         <div className='navBarComponent'>
@@ -12,13 +17,42 @@ const Navbar = () => {
             </div>
             <div className='navButtonContainer'>
                 <div className='navBarButtons'>
-                    <button className='navButton'>Character Info</button>
-                    <button className='navButton'>Races / Classes</button>
-                    <button className='navButton'>Equipment</button>
-                    <button className='navButton'>Magic</button>
-                    <button className='navButton'>Monsters</button>
-                    <button className='navButton'>NPCs</button>
-                    <button className='navButton'>Rules / Gameplay</button>
+                    <div className='navButton' id='navButton1' onClick={handleClick}>Character Info</div>
+                    <div className='navButtonPanel'>
+                        <p>Races</p>
+                        <p>Classes</p>
+                        <p>Ability Scores</p>
+                        <p>Skills</p>
+                        <p>Feats</p>
+                        <p>Alignment</p>
+                        <p>Background</p>
+                        <p>Languages</p>
+                    </div>
+                    <div className='navButton' id='navButton2' onClick={handleClick}>Equipment</div>
+                    <div className='navButtonPanel'>
+                        <p>Weapons</p>
+                        <p>Armor</p>
+                        <p>Adventuring Gear</p>
+                        <p>Magic Items</p>
+                        <p>Custom Items</p>
+                    </div>
+                    <div className='navButton' id='navButton3' onClick={handleClick}>Magic</div>
+                    <div className='navButtonPanel'>
+                        <p>Spellcasting</p>
+                        <p>Spell List</p>
+                    </div>
+                    <div className='navButton' id='navButton4' onClick={handleClick}>Monsters</div>
+                    <div className='navButtonPanel'>
+                        <p>Monster List</p>
+                    </div>
+                    <div className='navButton' id='navButton5' onClick={handleClick}>NPCs</div>
+                    <div className='navButtonPanel'>
+                        <p>Custom NPC Creator</p>
+                    </div>
+                    <div className='navButton' id='navButton6'onClick={handleClick}>Gameplay Mechanics</div>
+                    <div className='navButtonPanel'>
+                        <p>Calm down, we're working on it...</p>
+                    </div>
                 </div>
             </div>
         </div>
