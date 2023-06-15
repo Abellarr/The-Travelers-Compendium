06bcba320b325807e8b876342908c45f851a6fc8
Abellarr <da.ortiz83@gmail.com>
2023-06-15 14:53:14 -0700

    Added & logo for navbar with on click for home page. Removed unnecessary state from AppContext. Added more styling for the header and navbar components.

--- travelers-compendium/src/components/Context/AppContext.jsx
+++ travelers-compendium/src/components/Context/AppContext.jsx
@@ -3,14 +3,11 @@ import React, { useState, createContext } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [cohort, setCohort] = useState(null);
   const [bodyDisplay, setBodyDisplay] = useState('home');
 
   return (
     <AppContext.Provider
       value={{
-        cohort,
-        setCohort,
         bodyDisplay,
         setBodyDisplay,
       }}
--- travelers-compendium/src/components/Navbar/Navbar.jsx
+++ travelers-compendium/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React, { useContext } from 'react';
 import AppContext from '../Context/AppContext';
 import '../../styles/Navbar.css';
+import Logo from '../../../public/&-logo.png';
 
 const Navbar = () => {
-    const { bodyDisplay } = useContext(AppContext)
+    const { bodyDisplay, setBodyDisplay } = useContext(AppContext)
+
+    const handleDisplay = (e) => {
+        const bodyId = e.target.id;
+        setBodyDisplay(bodyId);
+    }
 
     const handleClick = (e) => {
         const button = document.getElementById(e.target.id);
@@ -16,46 +22,45 @@ const Navbar = () => {
     return (
         <div className='navBarComponent'>
             <div className='mainNavBar'>
-                <h1 className='mainTitle'><i>The Traveler's</i></h1>
-                <h1 className='mainTitle'><i>Compendium</i></h1>
+                <img src={Logo} alt='Ampersand Logo'className='navBarLogo' id='home' onClick={handleDisplay}></img>
             </div>
             <div className='navButtonContainer'>
                 <div className='navBarButtons'>
                     <div className='navButton' id='navButton1' onClick={handleClick}>Character Info</div>
                     <div className='navButtonPanel'>
-                        <p id='race'>Races</p>
-                        <p id='class'>Classes</p>
-                        <p id='ability'>Ability Scores</p>
-                        <p id='skill'>Skills</p>
-                        <p id='feat'>Feats</p>
-                        <p id='alignment'>Alignment</p>
-                        <p id='background'>Background</p>
-                        <p id='language'>Languages</p>
+                        <p id='race' onClick={handleDisplay}>Races</p>
+                        <p id='class' onClick={handleDisplay}>Classes</p>
+                        <p id='ability' onClick={handleDisplay}>Ability Scores</p>
+                        <p id='skill' onClick={handleDisplay}>Skills</p>
+                        <p id='feat' onClick={handleDisplay}>Feats</p>
+                        <p id='alignment' onClick={handleDisplay}>Alignment</p>
+                        <p id='background' onClick={handleDisplay}>Background</p>
+                        <p id='language' onClick={handleDisplay}>Languages</p>
                     </div>
                     <div className='navButton' id='navButton2' onClick={handleClick}>Equipment</div>
                     <div className='navButtonPanel'>
-                        <p id='weapon'>Weapons</p>
-                        <p id='armor'>Armor</p>
-                        <p id='adventGear'>Adventuring Gear</p>
-                        <p id='magItem'>Magic Items</p>
-                        <p id='custItem'>Custom Items</p>
+                        <p id='weapon' onClick={handleDisplay}>Weapons</p>
+                        <p id='armor' onClick={handleDisplay}>Armor</p>
+                        <p id='adventGear' onClick={handleDisplay}>Adventuring Gear</p>
+                        <p id='magItem' onClick={handleDisplay}>Magic Items</p>
+                        <p id='custItem' onClick={handleDisplay}>Custom Items</p>
                     </div>
                     <div className='navButton' id='navButton3' onClick={handleClick}>Magic</div>
                     <div className='navButtonPanel'>
-                        <p id='race'>Spellcasting</p>
-                        <p id='race'>Spell List</p>
+                        <p id='spellcasting' onClick={handleDisplay}>Spellcasting</p>
+                        <p id='spellList' onClick={handleDisplay}>Spell List</p>
                     </div>
                     <div className='navButton' id='navButton4' onClick={handleClick}>Monsters</div>
                     <div className='navButtonPanel'>
-                        <p id='race'>Monster List</p>
+                        <p id='monster' onClick={handleDisplay}>Monster List</p>
                     </div>
                     <div className='navButton' id='navButton5' onClick={handleClick}>NPCs</div>
                     <div className='navButtonPanel'>
-                        <p id='race'>Custom NPC Creator</p>
+                        <p id='custNPC' onClick={handleDisplay}>Custom NPC Creator</p>
                     </div>
                     <div className='navButton' id='navButton6'onClick={handleClick}>Gameplay</div>
                     <div className='navButtonPanel'>
-                        <p id='race'>Calm down, we're working on it...</p>
+                        <p id='rules' onClick={handleDisplay}>Calm down, we're working on it...</p>
                     </div>
                 </div>
             </div>
