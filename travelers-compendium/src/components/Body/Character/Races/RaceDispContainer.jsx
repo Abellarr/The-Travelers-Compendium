599c18725387bfb0aa4b3d3337bfe821eb4c9689
Abellarr <da.ortiz83@gmail.com>
2023-06-16 22:52:33 -0700

    Fixed issue with mapping in the Race display page.

--- travelers-compendium/src/components/Body/Character/Races/RaceDispContainer.jsx
+++ travelers-compendium/src/components/Body/Character/Races/RaceDispContainer.jsx
@@ -1,65 +1,74 @@
 import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../../../Context/AppContext.jsx';
 import '../../../../styles/Character.css';
-import RacesTopBar from './RacesTopBar.jsx';
-import RacesMain from './RacesMain.jsx';
 
 const RaceDispContainer = ({ raceDisplay }) => {
-    const [ raceInfo, setRaceInfo ] = useState({})
+    const [ raceInfo, setRaceInfo ] = useState({});
+    const [ traits, setTraits ] = useState([]);
+    const [ abilities, setAbilities ] = useState([])
     const { apiBase } = useContext(AppContext);
-
-    const traits = () => {
-        if (raceInfo.name) {
-            return raceInfo.traits;
-        }
-    }
 
     useEffect(() => {
         (async () => {
           const response = await fetch(`${apiBase}/api/races/${raceDisplay}`);
           const race = await response.json();
           setRaceInfo(race);
+          setTraits(race.traits);
+          setAbilities(race.ability_bonuses)
           console.log(race);
         })();
         return () => {};
       }, []);
 
     return (
-        <div className='racesDispContainer'>
-            <h1>{raceInfo.name}</h1>
-            <div>
-                <h2>Ability Score Increase</h2>
-                {raceInfo.name ? <p>{raceInfo.ability_bonuses[0].ability_score.name} - {raceInfo.ability_bonuses[0].bonus}</p> : null}
-                {raceInfo.name && raceInfo.ability_bonuses[1] ? <p>{raceInfo.ability_bonuses[1].ability_score.name} - {raceInfo.ability_bonuses[1].bonus}</p> : null}
-            </div>
-            <div>
-                <h2>Age</h2>
-                <p>{raceInfo.age}</p>
-            </div>
-            <div>
-                <h2>Alignment</h2>
-                <p>{raceInfo.alignment}</p>
-            </div>
-            <div>
-                <h2>Size</h2>
-                <h3>{raceInfo.size}</h3>
-                <p>{raceInfo.size_description}</p>
-            </div>
-            <div>
-                <h2>Speed</h2>
-                <p>Your base walking speed is {raceInfo.speed} feet</p>
-            </div>
-            <div>
-                <h2>Languages</h2>
-                <p>{raceInfo.language_desc}</p>
-            </div>
-            <div>
-                <h2>Traits</h2>
-                {traits.map((trait, ind)=>{
-                    return (
-                        <p key={`Trait${ind}`}>{trait.name}</p>
-                    )
-                })}
+        <div className='raceDispMain'>
+            <h1 className='raceInfoTitle'>{raceInfo.name}</h1>
+            <div className='raceInfoContainer'>
+                <div className='raceAbilities'>
+                    <h2>Ability Score Increase</h2>
+                    {raceInfo.name && raceInfo.ability_bonuses[0] ?
+                    <p className='raceAbilities'>{abilities.map((ability, ind)=>{
+                        return (
+                            <p key={`Trait${ind}`}>{ability.ability_score.name} - {ability.bonus}</p>
+                        )
+                    })}</p> 
+                    : null}
+                    {raceInfo.index === 'half-elf' ? <p>+1 to two ability scores of your choice (not Charisma)</p> : null}
+                </div>
+                <div>
+                    <h2>Age</h2>
+                    <p>{raceInfo.age}</p>
+                </div>
+                <div>
+                    <h2>Alignment</h2>
+                    <p>{raceInfo.alignment}</p>
+                </div>
+                <div>
+                    <h2>Size</h2>
+                    <h3>{raceInfo.size}</h3>
+                    <p>{raceInfo.size_description}</p>
+                </div>
+                <div>
+                    <h2>Speed</h2>
+                    <p>Your base walking speed is {raceInfo.speed} feet</p>
+                </div>
+                <div>
+                    <h2>Languages</h2>
+                    <p>{raceInfo.language_desc}</p>
+                </div>
+                <div>
+                    <h2>Traits</h2>
+                    {raceInfo.name ?
+                    <p className='raceTraits'>{traits.map((trait, ind)=>{
+                        return (
+                            <p key={`Trait${ind}`}>{trait.name}</p>
+                        )
+                    })}</p> : null}
+                </div>
+                <div>
+                    <h2>Subraces</h2>
+                    {raceInfo.name && raceInfo.subraces[0] ? <p className='subraceName'>{raceInfo.subraces[0].name}</p> : <p>None</p>}
+                </div>
             </div>
         </div>
     )
